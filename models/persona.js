47fe0e918d654bcf3126c12c2b39c6ddd3c16ff0
ccofrev <carlos.cofre@usach.cl>
2024-07-08 21:59:51 -0400

    Ultima versión

--- models/persona.js
+++ models/persona.js
@@ -1,18 +1,15 @@
 // models/persona.js
 module.exports = (sequelize, DataTypes) => {
-    const Persona = sequelize.define('Persona', {
-      nombre: DataTypes.STRING,
-      apellido: DataTypes.STRING,
-      apellido2: DataTypes.STRING,
-      telefono: DataTypes.STRING,
-      email: DataTypes.STRING,
-      estado: DataTypes.STRING
-    });
-  
-    Persona.associate = function(models) {
-      Persona.hasMany(models.Vehiculo, { foreignKey: 'persona_id' });
-    };
-  
-    return Persona;
-  };
-  
+  const Persona = sequelize.define('Persona', {
+    nombre: DataTypes.STRING,
+    apellido: DataTypes.STRING,
+    apellido2: DataTypes.STRING,
+    telefono: DataTypes.STRING,
+    email: DataTypes.STRING,
+    estado: DataTypes.STRING
+  }, {
+    tableName: 'Persona'  // Asegúrate de que el nombre de la tabla esté correcto aquí
+  });
+
+  return Persona;
+};

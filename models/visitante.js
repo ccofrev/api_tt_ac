e12999bc7040770dec0d7aae1021bd91013b8c10
ccofrev <carlos.cofre@usach.cl>
2024-07-10 22:59:33 -0400

    correcciones visitante

--- models/visitante.js
+++ models/visitante.js
@@ -1,21 +1,30 @@
-// models/visitante.js
 module.exports = (sequelize, DataTypes) => {
-    const Visitante = sequelize.define('Visitante', {
-      tipo: DataTypes.STRING,
-      residencia_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'Residencia',
-          key: 'id'
-        }
-      }
-    });
-  
-    Visitante.associate = function(models) {
-      Visitante.belongsTo(models.Residencia, { foreignKey: 'residencia_id' });
-      Visitante.belongsTo(models.Persona, { foreignKey: 'id' });
-    };
-  
-    return Visitante;
+  const Visitante = sequelize.define('Visitante', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    persona_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    tipo: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    residencia_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    tableName: 'Visitante' // Nombre correcto de la tabla
+  });
+
+  Visitante.associate = (models) => {
+    Visitante.belongsTo(models.Persona, { foreignKey: 'persona_id', as: 'persona' });
+    Visitante.belongsTo(models.Residencia, { foreignKey: 'residencia_id', as: 'residencia' });
   };
-  
+
+  return Visitante;
+};
--- routes/visitante.js
+++ routes/visitante.js
@@ -15,8 +15,20 @@ router.post('/', async (req, res) => {
 // Obtener todos los visitantes
 router.get('/', async (req, res) => {
   try {
-    const visitantes = await Visitante.findAll();
-    res.status(200).json(visitantes);
+    const { page = 1, perPage = 10 } = req.query;
+
+    const offset = (page - 1) * perPage;
+    const limit = parseInt(perPage, 10);
+
+    const { count, rows } = await Visitante.findAndCountAll({
+      offset,
+      limit
+    });
+
+    res.header('X-Total-Count', count.toString()); // Incluir el encabezado X-Total-Count
+    res.header('Content-Range', `visitante ${offset}-${offset + rows.length}/${count}`); // Incluir el encabezado Content-Range
+    res.header('Access-Control-Expose-Headers', 'X-Total-Count, Content-Range'); // Permitir que el encabezado sea accesible desde el cliente
+    res.status(200).json(rows);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }

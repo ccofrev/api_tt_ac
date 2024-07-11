424ce8c8b5460074d6971ad262db313c092aeae8
ccofrev <carlos.cofre@usach.cl>
2024-07-10 23:45:44 -0400

    correcciones acceso

--- models/acceso.js
+++ models/acceso.js
@@ -5,14 +5,6 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       autoIncrement: true
     },
-    persona_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Personas', // Asegúrate de que este nombre coincida con el nombre de la tabla 'Personas'
-        key: 'id'
-      },
-      allowNull: false
-    },
     metodo_acceso_id_1: {
       type: DataTypes.INTEGER,
       references: {
@@ -37,16 +29,67 @@ module.exports = (sequelize, DataTypes) => {
       },
       allowNull: true
     },
-    numero_barreras: {
+    nivel_seguridad: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: true
+    },
+    residente_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Personas',
+        key: 'id'
+      },
+      allowNull: true
+    },
+    visitante_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Visitantes',
+        key: 'id'
+      },
+      allowNull: true
+    },
+    trabajador_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Trabajadores',
+        key: 'id'
+      },
+      allowNull: true
+    },
+    dispositivo_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Dispositivos',
+        key: 'id'
+      },
+      allowNull: true
+    },
+    temporal: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    },
+    inicio_validez: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    fin_validez: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    estado: {
+      type: DataTypes.STRING,
+      allowNull: true
     }
   }, {
     tableName: 'Acceso' // Nombre correcto de la tabla
   });
 
   Acceso.associate = (models) => {
-    Acceso.belongsTo(models.Persona, { foreignKey: 'persona_id', as: 'persona' });
+    Acceso.belongsTo(models.Persona, { foreignKey: 'residente_id', as: 'residente' });
+    Acceso.belongsTo(models.Visitante, { foreignKey: 'visitante_id', as: 'visitante' });
+    Acceso.belongsTo(models.Trabajador, { foreignKey: 'trabajador_id', as: 'trabajador' });
+    Acceso.belongsTo(models.Dispositivo, { foreignKey: 'dispositivo_id', as: 'dispositivo' });
     Acceso.belongsTo(models.MetodoAcceso, { foreignKey: 'metodo_acceso_id_1', as: 'metodo_acceso_1' });
     Acceso.belongsTo(models.MetodoAcceso, { foreignKey: 'metodo_acceso_id_2', as: 'metodo_acceso_2' });
     Acceso.belongsTo(models.MetodoAcceso, { foreignKey: 'metodo_acceso_id_3', as: 'metodo_acceso_3' });
